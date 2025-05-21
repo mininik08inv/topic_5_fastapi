815bf89ed2649d515b8d6bd60c5e4477f61da23b
mininik08inv <mininik08inv@yandex.ru>
2025-05-21 18:52:08 +0300

    небольшой рефакторинг

--- src/databases/database.py
+++ src/databases/database.py
@@ -1,20 +1,32 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy import select, and_, desc
 
 from src.configs.config import config
+from src.models.trading_results_model import SpimexTradingResults
 
-
-# DATABASE_URL = f"postgresql+asyncpg://{config.db.DB_USER}:{config.db.DB_PASSWORD}@{config.db.DB_HOST}:{config.db.DB_PORT}/{config.db.DB_DATABASE}"
 DATABASE_URL = config.db.DB_URL
-
-class Base(DeclarativeBase):
-    pass
-
 
 engine = create_async_engine(DATABASE_URL, pool_size=30, max_overflow=20, pool_recycle=3600)
 
 new_session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
+
 async def get_session():
     async with new_session() as session:
         yield session
+
+
+async def base_query(session: AsyncSession, filters=None, order_by=None, limit=None):
+    stmt = select(SpimexTradingResults)
+
+    if filters:
+        stmt = stmt.where(and_(*filters))
+
+    if order_by is not None:
+        stmt = stmt.order_by(order_by)
+
+    if limit is not None:
+        stmt = stmt.limit(limit)
+
+    result = await session.execute(stmt)
+    return result.scalars().all()
